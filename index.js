9cde5a68f7d2ee659d8d40b201f1c3f2f5edfdcf
Richa Verma <bansalr@us.ibm.com>
2017-10-26 12:05:46 -0500

    Updated server path to /status and added morgan

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/', function(req, res){
+app.use(morgan('combined'));
+app.get('/status', function(req, res){
   res.send('hello world');
 });
-
 app.listen(3001, () => {
   console.log('Server started at 3001');
 });
